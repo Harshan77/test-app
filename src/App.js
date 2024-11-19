a7894c76983801f6c8426bf13dde819cc3b14cb3
harshan77-admin <harshanveerabathini777@gmail.com>
2024-11-19 23:28:30 +0530

    changed color and text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Learn React
+        This is I'm changing git v1 Learn React
         </a>
       </header>
     </div>
